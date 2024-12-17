2bc86940ef1d870799b71c16e05917192717cf4a
abusumi <hpbd12201204@gmail.com>
2024-12-17 15:05:56 +0900

    fix:dexise_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { general: 0, admin: 1 } # 役割の定義
+
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
