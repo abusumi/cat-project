8c307e1712c80f3e4b3c3d2406b07587c6f15a28
abusumi <hpbd12201204@gmail.com>
2025-01-12 03:26:16 +0900

    fix:警告に対する修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def safe_external_url(url)
     uri = URI.parse(url)
-    uri.is_a?(URI::HTTP) && !uri.host.nil? ? url : '#'
+    uri.is_a?(URI::HTTP) && !uri.host.nil? ? url : "#"
   rescue URI::InvalidURIError
-    '#'
+    "#"
   end
 end
