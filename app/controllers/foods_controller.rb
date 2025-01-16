b335c502f8740196ffd757cd74a3c47a4fc826fb
abusumi <hpbd12201204@gmail.com>
2025-01-17 06:19:21 +0900

    add:total_reviews_etc

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,9 @@
 class FoodsController < ApplicationController
   def show
     @food = Food.find(params[:id])
+    @average_rating = @food.average_rating
+    @total_reviews = @food.total_reviews
+    @rating_percentage = @food.rating_percentage
   end
 
   def bookmarks
--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,21 @@ class Food < ApplicationRecord
   has_many :feeding_calculation
   has_many :bookmarks, dependent: :destroy
   has_many :bookmarked_users, through: :bookmarks, source: :user
+
+  def average_rating
+    reviews.average(:rating)&.round(1) || 0 # nilの場合は0を返す
+  end
+
+  def total_reviews
+    reviews.count
+  end
+
+  def rating_percentage
+    total = total_reviews.to_f
+    return { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0} if total.zero?
+
+    (1..5).each_with_object({}) do |star, percecntages|
+      percecntages[star] = ((reviews.where(rating: star).count / total) * 100).round
+    end
+  end
 end
