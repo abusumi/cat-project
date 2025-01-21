e8478a22d26e6be9c1920f58c8b2a0910a6991e6
abusumi <hpbd12201204@gmail.com>
2025-01-21 19:00:00 +0900

    add:user_profile_update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @last_calculation = current_user.feeding_calculations.includes(:main_food, :cat).order(created_at: :desc).first  end
+    @last_calculation = current_user.feeding_calculations.includes(:main_food, :cat).order(created_at: :desc).first
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user, notice: 'ユーザー情報が更新されました'
+    else
+      flash.now[:alert] = @user.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :self_introduction)
+  end
 end
