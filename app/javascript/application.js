1518b194a2f5dbb9e896b1b9a834d340c316c3a7
abusumi <hpbd12201204@gmail.com>
2025-01-30 19:00:00 +0900

    fix:error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import "../stylesheets/application.scss"
 import "./controllers"
 import "./packs/brand_food_filter";
