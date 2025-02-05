47801d1362eaa4a8b8e65fe267a69d7c328e4cba
abusumi <hpbd12201204@gmail.com>
2025-02-05 19:00:00 +0900

    fix:google_sign_in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,6 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
-  OmniAuth.config.allowed_request_methods = %i[get]
   
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
