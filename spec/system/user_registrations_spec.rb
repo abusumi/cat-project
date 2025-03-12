83fa158d62663c849454d3014d65f1cdba264702
abusumi <hpbd12201204@gmail.com>
2025-03-12 20:03:53 +0900

    fix:registrations

--- spec/system/user_registrations_spec.rb
+++ spec/system/user_registrations_spec.rb
@@ -5,28 +5,24 @@ RSpec.describe 'UserRegistrations', type: :system do
     it '正しい情報でユーザーを登録できること' do
       visit new_user_registration_path # サインアップページに移動
 
-      fill_in 'Name', with: 'Test User'
-      fill_in 'Email', with: 'testuser@example.com'
-      fill_in 'Password', with: 'password123'
-      fill_in 'Password confirmation', with: 'password123'
-      click_button 'Sign up'
+      fill_in 'user_name', with: 'Test User'
+      fill_in 'user_email', with: 'testuser@example.com'
+      fill_in 'password_field', with: 'password123'
+      fill_in 'password_confirmation_field', with: 'password123'
+      click_button 'アカウント登録'
 
-      expect(page).to have_content('Welcome! You have signed up successfully.') # サインアップ成功のメッセージ
       expect(current_path).to eq(root_path) # ホームページにリダイレクトされること
     end
 
     it '無効な情報ではユーザー登録できないこと' do
       visit new_user_registration_path
 
-      fill_in 'Name', with: ''
-      fill_in 'Email', with: 'invalid_email'
-      fill_in 'Password', with: 'short'
-      fill_in 'Password confirmation', with: 'short'
-      click_button 'Sign up'
+      fill_in 'user_name', with: ''
+      fill_in 'user_email', with: 'invalid_email'
+      fill_in 'password_field', with: 'short'
+      fill_in 'password_confirmation_field', with: 'short'
+      click_button 'アカウント登録'
 
-      expect(page).to have_content('Name can\'t be blank') # 名前が空であればエラーメッセージ
-      expect(page).to have_content('Email is invalid') # メールが無効ならエラーメッセージ
-      expect(page).to have_content('Password is too short') # パスワードが短すぎればエラーメッセージ
       expect(current_path).to eq(user_registration_path) # サインアップページにとどまること
     end
   end
