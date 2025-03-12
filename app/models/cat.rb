5385d4c44514a0d7a9cf530fd635d6b732699be0
abusumi <hpbd12201204@gmail.com>
2025-03-12 11:10:45 +0900

    add:validate

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,7 @@
 class Cat < ApplicationRecord
   belongs_to :user
   has_one_attached :cat_profile
+
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :cat_introduction, length: {in: 0..250}
 end
