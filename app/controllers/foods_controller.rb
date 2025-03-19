943cabc159a55c8583d8c353f4dab6f76b6bf9c8
abusumi <hpbd12201204@gmail.com>
2025-03-20 05:08:03 +0900

    fix:food_show

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,7 @@
 class FoodsController < ApplicationController
   def show
     @food = Food.find(params[:id])
+    @reviews = @food.reviews.includes(:user).order(created_at: :desc)
     @average_rating = @food.average_rating
     @total_reviews = @food.total_reviews
     @rating_percentage = @food.rating_percentage
