9bf832c525fd79fb5cb8fb9eb03c7ff2370211c6
abusumi <hpbd12201204@gmail.com>
2025-05-13 19:00:00 +0900

    add:cats_spec_test

---
+++ spec/system/cats_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe "Cats", type: :system do
+  let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:cat) { create(:cat, user: user) }
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  def login(user)
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'password_field', with: user.password
+    click_button 'ログイン'
+  end
+
+  describe "猫の一覧ページ" do
+    it "一覧に猫の名前が表示される" do
+      visit cats_path
+      expect(page).to have_content(cat.name)
+    end
+  end
+
+  describe "猫の詳細ページ" do
+    it "詳細情報が表示される" do
+      visit user_cat_path(user_id: cat.user_id, id: cat.id)
+      expect(page).to have_content(cat.name)
+      expect(page).to have_content(cat.cat_introduction)
+    end
+  end
+
+  describe "猫の登録" do
+    it "ログインしたユーザーが猫を登録できる" do
+      login(user)
+
+      visit new_user_cat_path(user)
+      fill_in 'cat_name', with: 'ねこ'
+      fill_in 'cat_weight', with: '4.2'
+      fill_in 'cat_cat_introduction', with: '元気な猫です'
+
+      click_button '登録する'
+
+      expect(page).to have_content('猫が登録されました')
+      expect(page).to have_content('ペット')
+    end
+  end
+
+  describe "猫の編集" do
+    it "作成者が猫の情報を編集できる" do
+      login(user)
+
+      visit edit_user_cat_path(user, cat)
+      fill_in 'cat_name', with: '編集後のペット'
+      click_button '登録する'
+
+      expect(page).to have_content('情報が更新されました')
+      expect(page).to have_content('編集後のペット')
+    end
+
+    it "他のユーザーは編集ページにアクセスできない" do
+      login(other_user)
+
+      visit edit_user_cat_path(user, cat)
+      expect(current_path).to eq(cats_path)
+      expect(page).to have_content('権限がありません')
+    end
+  end
+end
